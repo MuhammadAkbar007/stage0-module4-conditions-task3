d55261af6ad4a1135bf4de531c851e8cb7dba21d
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-03-28 16:56:44 +0500

    Bismillah 🥇

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        IsEnglishSymbolDeterminer isEnglishSymbolDeterminer = new IsEnglishSymbolDeterminer();
+        if (!isEnglishSymbolDeterminer.check(character)) {
+            System.out.println("wrong alphabet!");
+        } else {
+            System.out.println(character == 'A' || character == 'a' || character == 'O' || character == 'o' ||
+                    character == 'E' || character == 'e' || character == 'I' || character == 'i' || character == 'U' ||
+                    character == 'u' || character == 'Y' || character == 'y' ? "Vowel" : "Consonant");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        System.out.println(check(symbol) ? "English" : "Non English");
+    }
+
+    public boolean check(char symbol) {
+        return (symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z');
     }
 }
